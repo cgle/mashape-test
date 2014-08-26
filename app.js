de7820e80cae985bf8c7666b13776fd6e867b5f0
cgle <cle@macalester.edu>
2014-08-26 10:39:05 -0700

    change port

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ db.on('open', function() {
 });
 
 var runService = function() {
-  app.set('port', process.env.PORT || 5000);
+  app.set('port', process.env.PORT || 8080);
   app.use(express.logger('dev'));
   app.use(express.json({limit: '50mb'}));
   app.use(express.urlencoded({limit: '50mb'}));
@@ -319,5 +319,5 @@ var runService = function() {
     });
   });
 
-  app.listen(5000);
+  app.listen(8080);
 }
